4cd8c4a6297791700e82fa7897d84f43d1de2a9a
Antoinoe <noeguillaumin@gmail.com>
2023-03-27 00:18:22 +0200

    Project - Clean : Deleted USELESS code

--- Assets/Scripts/APIConnector.cs
+++ Assets/Scripts/APIConnector.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Leap.Unity;
 using UnityEngine;
@@ -10,52 +8,14 @@ public class APIConnector : MonoBehaviour
 {
     //HOLOGRAM
     private Content _hologramContent;
-
     //LEAP MOTION
     [SerializeField] private HandModelBase rightHandModel, leftHandModel;
-    [SerializeField] private float swipeSpeed = 2.0f;
-    [SerializeField] private float swipeDistance = 2.0f;
-    [SerializeField] private bool _isRightHandSwiping, _isLeftHandSwiping;
-    [SerializeField] private Vector3 _rightSwipeStartPos, _leftSwipeStartPos;
-
     //ESP32
     [SerializeField] private string ip;
-    [SerializeField] private string url;
-    [SerializeField] private int pin;
-
-    [SerializeField] private bool pinStatus;
     [SerializeField] private string editorMessage;
-
-    [SerializeField] private bool canInput = true;
-    private bool isHandShowed;
-    private bool wasHandShowed;
-
-    enum pinMode
-    {
-        INPUT,
-        OUTPUT
-    };
-    enum pinNumber
-    {
-        D1,
-        D2,
-        D3,
-        D4
-    };
-    enum pinState
-    {
-        HIGH,
-        LOW
-    };
-    [Serializable] private struct Pin
-    {
-        public string name;
-        public pinMode pinMode;
-        public pinNumber pinNumber;
-        public pinState pinState;
-    }
-    [SerializeField] private List<Pin> pins = new List<Pin>();
-
+    
+    private bool _isHandShowed;
+    private bool _wasHandShowed;
     void Start()
     {
         _hologramContent = FindObjectOfType<Content>();
@@ -65,13 +25,12 @@ public class APIConnector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         CheckForHands();
-        //CheckForUserInputs();
     }
 
     public void OnButtonClicked()
     {
+        //just a debug function, there was a button in the scene back in the days...
         StartCoroutine(SendRequest(editorMessage));
     }
 
@@ -80,8 +39,8 @@ public class APIConnector : MonoBehaviour
         if (leftHandModel.isActiveAndEnabled || rightHandModel.isActiveAndEnabled)
         {
             
-            isHandShowed = true;
-            if (!wasHandShowed)
+            _isHandShowed = true;
+            if (!_wasHandShowed)
             {
                 //user has its hands visible by the LeapMotion Device
                 StartCoroutine(SendRequest("OnLed3"));
@@ -91,13 +50,13 @@ public class APIConnector : MonoBehaviour
         else
         {
             //user has its hands out of the LeapMotion Device's vision
-            isHandShowed = false;
-            if(wasHandShowed) StartCoroutine(SendRequest("OffLed3"));
+            _isHandShowed = false;
+            if(_wasHandShowed) StartCoroutine(SendRequest("OffLed3"));
         }
-        wasHandShowed = isHandShowed;
+        _wasHandShowed = _isHandShowed;
     }
 
-    public IEnumerator SendRequest(string msg, [Optional] int delay)
+    public IEnumerator SendRequest(string msg, [Optional] float delay)
     {
         //delay mainly use when turning all leds off in the beginning 
         yield return new WaitForSeconds(delay);
@@ -116,79 +75,11 @@ public class APIConnector : MonoBehaviour
             Debug.Log($"{answer}");
         }
     }
-
-    private void CheckForUserInputs()
-    {
-        //Handle Left
-        HandleLeft();
-        HandleRight();
-        
-        
-        //Handle Right
-        
-    }
-
-    private void HandleLeft()
-    {
-        try
-        {
-            var leftHand = leftHandModel.GetLeapHand();
-            if (!leftHandModel) return;
-            print(leftHand.PalmPosition.ToString());
-        
-            if (leftHand.IsLeft && leftHand.PalmVelocity.x > swipeSpeed && !_isLeftHandSwiping)
-            {
-                _isLeftHandSwiping = true;
-                _leftSwipeStartPos = leftHand.PalmPosition;
-            }
-            // Check if the swipe gesture has ended
-            else if (_isLeftHandSwiping && Vector3.Distance(_leftSwipeStartPos, leftHand.PalmPosition) < -swipeDistance)
-            {
-                _isLeftHandSwiping = false;
-                // Trigger your event here
-                Debug.Log("Left hand swiped to the left!");
-                _hologramContent.ActivateItem(_hologramContent.GetPreviousItem());
-            }
-        }
-        catch
-        {
-            //ignore
-        }
-    }
-
-    private void HandleRight()
-    {
-        try
-        {
-            var rightHand = rightHandModel.GetLeapHand();
-            if (!rightHandModel) return;
-        
-            if (rightHand.IsRight && rightHand.PalmVelocity.x > swipeSpeed && !_isRightHandSwiping)
-            {
-                _isRightHandSwiping = true;
-                _rightSwipeStartPos = rightHand.PalmPosition;
-            }
-            // Check if the swipe gesture has ended
-            else if (_isRightHandSwiping && Vector3.Distance(_rightSwipeStartPos, rightHand.PalmPosition) > swipeDistance)
-            {
-                _isRightHandSwiping = false;
-                // Trigger your event here
-                Debug.Log("Right hand swiped to the right!");
-                _hologramContent.ActivateItem(_hologramContent.GetNextItem());
-            }
-        }
-        catch
-        {
-            //ignore
-        }
-
-    }
-
     private void ResetAll()
     {
-        StartCoroutine(SendRequest("OffLed1"));
-        StartCoroutine(SendRequest("OffLed2"));
-        StartCoroutine(SendRequest("OffLed3"));
+        StartCoroutine(SendRequest("OffLed1",0.5f));
+        StartCoroutine(SendRequest("OffLed2",0.5f));
+        StartCoroutine(SendRequest("OffLed3",0.5f));
     }
 
  }
--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
-    [SerializeField] 
-    private float leapSwipeDelay = 2.0f;
-    
     private Content _content;
     private LeapServiceProvider _leap;
     private bool _isConnectedToLeap;
@@ -18,7 +15,7 @@ public class UserInput : MonoBehaviour
         _content = FindObjectOfType<Content>();
         _leap = FindObjectOfType<LeapServiceProvider>();
         _isConnectedToLeap = _leap.IsConnected();
-        print(_isConnectedToLeap);
+        print(_isConnectedToLeap.ToString());
         _canLeapSwipeLeft = true;
         _canLeapSwipeRight = true;
     }
@@ -26,7 +23,6 @@ public class UserInput : MonoBehaviour
     private void Update()
     {
         HandleKeyboard();
-        StartCoroutine(HandleLeapMotion());
     }
 
     private void HandleKeyboard()
@@ -35,27 +31,5 @@ public class UserInput : MonoBehaviour
             _content.GetCurrentItem().RotateByUser();
         if (Input.GetKeyDown(KeyCode.RightArrow))
             _content.ActivateItem(_content.GetNextItem());
-    }
-
-    private IEnumerator HandleLeapMotion()
-    {
-        
-        if (!_isConnectedToLeap)yield break;
-        if (_canLeapSwipeLeft)
-        {
-            _canLeapSwipeLeft = false;
-            print("left");
-            //code here
-            yield return new WaitForSeconds(leapSwipeDelay);
-            _canLeapSwipeLeft = true;
-        }
-        if (_canLeapSwipeRight)
-        {
-            _canLeapSwipeRight = false;
-            print("right");
-            //code here
-            yield return new WaitForSeconds(leapSwipeDelay);
-            _canLeapSwipeRight = true;
-        }
     }
 }
